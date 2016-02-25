eaa07281b810da117c10d18d16cd2a5ac3e168b0
Walter Oggioni <oggioni.walter@gmail.com>
2016-02-26 00:46:58 +0100

    correctly managed db re-initialization

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ WILDFLY_PWD="123456"
 mkdir /run/postgresql
 chown postgres:postgres /run/postgresql
 
-if [[ $(cat /root/booted.txt) -eq 0 ]]; then
+if [[ $(ls /var/lib/postgres/data/ | wc -l) -eq 0 ]]; then
     echo First run: adding admin user to wildfly and creating postgres database
     su wildfly -c "/opt/wildfly/bin/add-user.sh admin $WILDFLY_PWD"
     su postgres -c "initdb --locale en_US.UTF-8 -E UTF8 -D '/var/lib/postgres/data'"
@@ -13,7 +13,6 @@ if [[ $(cat /root/booted.txt) -eq 0 ]]; then
     su postgres -c "createuser wildfly"
     su postgres -c "createdb wildfly -O wildfly"
     su postgres -c "/usr/bin/pg_ctl -s -D /var/lib/postgres/data stop -m fast"
-    echo 1 > /root/booted.txt
 fi
 
 /usr/bin/supervisord -n
